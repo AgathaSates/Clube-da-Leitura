559c367d1900b579b2bbb6e6779db8d2048bf0b6
Agatha Sates <satesagatha@gmail.com>
2025-04-10 20:56:00 -0300

    feat: Adicionado classe TelaPrincipal

---
+++ Clube-da-Leitura.ConsoleApp/Utilitarios/TelaPincipal.cs
@@ -0,0 +1,23 @@
+﻿namespace Clube_da_Leitura.ConsoleApp.Utilitarios;
+
+class TelaPincipal
+{
+    public static void ApresentarMenuPrincipal()
+    {
+        Console.Clear();
+        Console.WriteLine("╔══════════════════════════════════╗");
+        Console.WriteLine("║         Clube da Leitura         ║");
+        Console.WriteLine("║══════════════════════════════════║");
+        Console.WriteLine("║  Bem-vindo ao Clube da Leitura!  ║");
+        Console.WriteLine("║══════════════════════════════════║");
+        Console.WriteLine("║ 1. Gerenciar Amigos.             ║");
+        Console.WriteLine("║ 2. Gerenciar Revistas.           ║");
+        Console.WriteLine("║ 3. Gerenciar Caixas.             ║");
+        Console.WriteLine("║ 4. Gerenciar Empréstimos.        ║");
+        Console.WriteLine("║ 5. Sair do Clube.                ║");
+        Console.WriteLine("╚══════════════════════════════════╝");
+        Console.Write("> Digite uma opção: ");
+        Console.ReadLine();
+    }
+    
+}
