b83d75f53f3f1cc89ce55bdca678078b60c4296e
Agatha Sates <satesagatha@gmail.com>
2025-04-11 20:57:15 -0300

    feat: Implementado Classe Revista

--- Clube-da-Leitura.ConsoleApp/ModuloRevista/Revista.cs
+++ Clube-da-Leitura.ConsoleApp/ModuloRevista/Revista.cs
@@ -1,5 +1,58 @@
-﻿namespace Clube_da_Leitura.ConsoleApp.ModuloRevista;
+﻿using Clube_da_Leitura.ConsoleApp.ModuloCaixa;
+
+namespace Clube_da_Leitura.ConsoleApp.ModuloRevista;
 class Revista
 {
+    public int id;
+    public string titulo;
+    public int numeroDaEdicao;
+    public int anoDaPublicacao;
+    public string statusDeEmprestimo;
+    public Caixa caixa;
 
+    //● Não pode haver revistas com mesmo título e edição
+
+    public Revista(string titulo, int numeroDaEdicao, int anoDaPublicacao, Caixa caixa)
+    {
+        this.titulo = titulo;
+        this.numeroDaEdicao = numeroDaEdicao;
+        this.anoDaPublicacao = anoDaPublicacao;
+        statusDeEmprestimo = "Disponível";
+        this.caixa = caixa;
+    }
+    
+    public string Validar()
+    {
+        int tamanhoMaximoTitulo = 100;
+        int tamanhoMinimoTitulo = 2;
+        string erros = "";
+
+        if (string.IsNullOrWhiteSpace(titulo))
+            erros += "> O Título é obrigatório!\n";
+
+        else if (titulo.Length < tamanhoMinimoTitulo || titulo.Length > tamanhoMaximoTitulo)
+            erros += "> O Título deve ter entre 2 e 100 caracteres\n";
+
+        if (numeroDaEdicao <= 0)
+            erros += "> O Número da Edição deve ser um número positivo\n";
+
+        if (anoDaPublicacao < 1800 || anoDaPublicacao > DateTime.Now.Year)
+            erros += "> O Ano da Publicação deve ser um ano válido\n";
+
+        if (caixa == null)
+            erros += "> A Caixa é obrigatória!\n";
+
+
+        return erros;
+    }
+
+    public void Emprestar()
+    {
+        statusDeEmprestimo = "Emprestada";
+    }
+
+    public void Devolver()
+    {
+        statusDeEmprestimo = "Disponível";
+    }
 }
