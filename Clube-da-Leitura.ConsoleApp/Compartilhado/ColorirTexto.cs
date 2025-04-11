742df1c0116f03bbaf84981bd09399ca90769905
Agatha Sates <satesagatha@gmail.com>
2025-04-11 19:50:55 -0300

    feat: Implementado Classe Colorir texto

---
+++ Clube-da-Leitura.ConsoleApp/Compartilhado/ColorirTexto.cs
@@ -0,0 +1,18 @@
+﻿namespace Clube_da_Leitura.ConsoleApp.Compartilhado;
+
+class ColorirTexto
+{
+    public static void ExibirMensagemSemLinha(string mensagem, ConsoleColor cor) 
+    {
+        Console.ForegroundColor = cor;
+        Console.Write(mensagem);
+        Console.ResetColor();
+    }
+
+    public static void ExibirMensagem(string mensagem, ConsoleColor cor)
+    {
+        Console.ForegroundColor = cor;
+        Console.WriteLine(mensagem);
+        Console.ResetColor();
+    }
+}
