0a9ea95edc9e672a1ad95dfb1090cef518a07a5f
Agatha Sates <satesagatha@gmail.com>
2025-04-11 19:52:42 -0300

    feat: Implementado Gerador de Ids

---
+++ Clube-da-Leitura.ConsoleApp/Compartilhado/GeradorDeIDs.cs
@@ -0,0 +1,30 @@
+﻿namespace Clube_da_Leitura.ConsoleApp.Compartilhado;
+
+class GeradorDeIDs
+{
+    public static int idAmigo = 0;
+    public static int idRevista = 0;
+    public static int idCaixa = 0;
+    public static int idEmprestimo = 0;
+
+    public static int GerarIdAmigo()
+    {
+        return ++idAmigo;
+    }
+
+    public static int GerarIdRevista()
+    {
+        return ++idRevista;
+    }
+
+    public static int GerarIdCaixa()
+    {
+        return ++idCaixa;
+    }
+
+    public static int GerarIdEmprestimo()
+    {
+        return ++idEmprestimo;
+    }
+
+}
