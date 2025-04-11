7d82f92efb5e6f8fef644400ef5bcd992290a23b
Agatha Sates <satesagatha@gmail.com>
2025-04-10 21:55:01 -0300

    feat: Implementado Menu Principal do programa

--- Clube-da-Leitura.ConsoleApp/Utilitarios/TelaPincipal.cs
+++ Clube-da-Leitura.ConsoleApp/Utilitarios/TelaPincipal.cs
@@ -1,23 +1,35 @@
 ﻿namespace Clube_da_Leitura.ConsoleApp.Utilitarios;
 
-class TelaPincipal
+class telaPincipal
 {
-    public static void ApresentarMenuPrincipal()
+    public void ApresentarMenuPrincipal()
     {
         Console.Clear();
-        Console.WriteLine("╔══════════════════════════════════╗");
-        Console.WriteLine("║         Clube da Leitura         ║");
-        Console.WriteLine("║══════════════════════════════════║");
-        Console.WriteLine("║  Bem-vindo ao Clube da Leitura!  ║");
-        Console.WriteLine("║══════════════════════════════════║");
-        Console.WriteLine("║ 1. Gerenciar Amigos.             ║");
-        Console.WriteLine("║ 2. Gerenciar Revistas.           ║");
-        Console.WriteLine("║ 3. Gerenciar Caixas.             ║");
-        Console.WriteLine("║ 4. Gerenciar Empréstimos.        ║");
-        Console.WriteLine("║ 5. Sair do Clube.                ║");
-        Console.WriteLine("╚══════════════════════════════════╝");
+        Console.WriteLine("╔════════════════════════════════╗");
+        Console.WriteLine("║        Clube da Leitura        ║");
+        Console.WriteLine("║════════════════════════════════║");
+        Console.WriteLine("║ Bem-vindo ao Clube da Leitura! ║");
+        Console.WriteLine("║════════════════════════════════║");
+        Console.WriteLine("║ 1. Gerenciar Amigos.           ║");
+        Console.WriteLine("║ 2. Gerenciar Revistas.         ║");
+        Console.WriteLine("║ 3. Gerenciar Caixas.           ║");
+        Console.WriteLine("║ 4. Gerenciar Empréstimos.      ║");
+        Console.WriteLine("║ 5. Sair do Clube.              ║");
+        Console.WriteLine("╚════════════════════════════════╝");
         Console.Write("> Digite uma opção: ");
-        Console.ReadLine();
     }
-    
+
+    public void ApresentarSairDoClube()
+    {
+        Console.Clear();
+        Console.WriteLine("╔═════════════════════════════════════════╗");
+        Console.WriteLine("║  Obrigado por usar o Clube da Leitura!  ║");
+        Console.WriteLine("║              Até a próxima!             ║");
+        Console.WriteLine("╚═════════════════════════════════════════╝");
+    }
+
+    public void ApresentarOpçaoInvalida()
+    {
+        Console.WriteLine("Opção inválida! Digite novamente: ");
+    }
 }
