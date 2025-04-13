49cf3eb89a8d56e62ddd9c41e0128f20518ee68c
Agatha Sates <satesagatha@gmail.com>
2025-04-13 04:24:15 -0300

    feat: Implementada Classe estática Validador

---
+++ Clube-da-Leitura.ConsoleApp/Compartilhado/Validador.cs
@@ -0,0 +1,25 @@
+﻿namespace Clube_da_Leitura.ConsoleApp.Compartilhado;
+
+class Validador
+{
+
+    public static bool VerificaSeEhNumero(string texto)
+    {
+        foreach (char c in texto)
+        {
+            if (!char.IsDigit(c))
+                return false;
+        }
+        return true;
+    }
+
+    public static int DigitouUmNumero()
+    {
+        int ehNumero;
+
+        while (!int.TryParse(Console.ReadLine(), out ehNumero))
+        {   ColorirTexto.ExibirMensagemSemLinha("O Valor precisa ser número! Digite novamente:", ConsoleColor.Red);  
+        }
+        return ehNumero;
+    }
+}
