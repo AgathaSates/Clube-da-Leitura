50990080834fcf55f78a455c7a4070dd8f014221
Agatha Sates <satesagatha@gmail.com>
2025-04-14 05:43:59 -0300

    style: Apagado linhas em branco

--- Clube-da-Leitura.ConsoleApp/Compartilhado/Validador.cs
+++ Clube-da-Leitura.ConsoleApp/Compartilhado/Validador.cs
@@ -2,14 +2,12 @@
 
 class Validador
 {
-
     public static bool VerificaSeEhNumero(string texto)
     {
         foreach (char c in texto)
-        {
             if (!char.IsDigit(c))
                 return false;
-        }
+
         return true;
     }
 
@@ -18,8 +16,8 @@ class Validador
         int ehNumero;
 
         while (!int.TryParse(Console.ReadLine(), out ehNumero))
-        {   ColorirTexto.ExibirMensagemSemLinha("O Valor precisa ser número! Digite novamente:", ConsoleColor.Red);  
-        }
+           ColorirTexto.ExibirMensagemSemLinha("O Valor precisa ser número! Digite novamente:", ConsoleColor.Red);  
+
         return ehNumero;
     }
 }
