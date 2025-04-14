e95db1f5af8ad655f6103649ff0d0d1884d1b910
Agatha Sates <satesagatha@gmail.com>
2025-04-14 05:50:39 -0300

    refactor: Program refatorada para mais clareza

--- Clube-da-Leitura.ConsoleApp/Program.cs
+++ Clube-da-Leitura.ConsoleApp/Program.cs
@@ -1,44 +1,11 @@
-﻿using Clube_da_Leitura.ConsoleApp.ModuloAmigo;
-using Clube_da_Leitura.ConsoleApp.ModuloCaixa;
-using Clube_da_Leitura.ConsoleApp.ModuloEmprestimo;
-using Clube_da_Leitura.ConsoleApp.ModuloRevista;
-using Clube_da_Leitura.ConsoleApp.Utilitarios;
+﻿using Clube_da_Leitura.ConsoleApp.Utilitarios;
 namespace Clube_da_Leitura.ConsoleApp;
 
 internal class Program
 {
     static void Main(string[] args)
-    {
-        RepositorioAmigo repositorioAmigo = new RepositorioAmigo();
-        RepositorioRevista repositorioRevista = new RepositorioRevista();
-        RepositorioCaixa repositorioCaixa = new RepositorioCaixa();
-        RepositorioEmprestimo repositorioEmprestimo = new RepositorioEmprestimo();
-
-        TelaAmigo telaAmigo = new TelaAmigo(repositorioAmigo);
-        TelaRevista telaRevista = new TelaRevista(repositorioRevista);
-        TelaCaixa telaCaixa = new TelaCaixa(repositorioCaixa);
-        TelaEmprestimo telaEmprestimo = new TelaEmprestimo(repositorioEmprestimo, repositorioAmigo, repositorioRevista, repositorioCaixa);
-        telaPincipal telaPincipal = new telaPincipal();
-
-        while (true) 
-        {
-            Console.Title = "Clube da Leitura";
-            telaPincipal.ApresentarMenuPrincipal();
-            string opcaomenu = Console.ReadLine();
-            switch (opcaomenu)
-            {
-                case "1": telaAmigo.ApresentarMenu(); break;
-
-                case "2": telaRevista.ApresentarMenu(); break;
-
-                case "3": telaCaixa.ApresentarMenu(); break;
-
-                case "4": telaEmprestimo.ApresentarMenu(); break;
-
-                case "5": telaPincipal.ApresentarSairDoClube(); return;
-
-                default: telaPincipal.ApresentarOpçaoInvalida(); break;
-            }
-        }      
+    {      
+        TelaPincipal telaPincipal = new TelaPincipal();
+        telaPincipal.ApresentarMenuPrincipal();
     }
 }
