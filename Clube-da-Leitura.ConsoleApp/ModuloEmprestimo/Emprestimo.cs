4cbc87e62e4f4f1d16a97d14d99e28770581249d
Agatha Sates <satesagatha@gmail.com>
2025-04-14 05:47:58 -0300

    feat: Implementado Classe Emprestimo

--- Clube-da-Leitura.ConsoleApp/ModuloEmprestimo/Emprestimo.cs
+++ Clube-da-Leitura.ConsoleApp/ModuloEmprestimo/Emprestimo.cs
@@ -1,5 +1,70 @@
-﻿namespace Clube_da_Leitura.ConsoleApp.ModuloEmprestimo;
+﻿using Clube_da_Leitura.ConsoleApp.Compartilhado;
+using Clube_da_Leitura.ConsoleApp.ModuloAmigo;
+using Clube_da_Leitura.ConsoleApp.ModuloRevista;
+
+namespace Clube_da_Leitura.ConsoleApp.ModuloEmprestimo;
 class Emprestimo
 {
+    public int Id;
+    public Amigo amigo;
+    public Revista revista;
+    public DateTime DataIniciodoEmprestimo;
+    public DateTime DataDevolucao;
+    public string StatusDeEmprestimo; //Aberto / Concluído / Atrasado
 
+    public Emprestimo(Amigo amigo, Revista revista, DateTime dataEmprestimo)
+    {
+        Id = GeradorDeIDs.GerarIdEmprestimo();
+        this.amigo = amigo;
+        this.revista = revista;
+        DataIniciodoEmprestimo = dataEmprestimo;
+        DataDevolucao = ObterDataDeDevolucao();
+
+    }
+
+    public string Validar()
+    {
+        string erros = "";
+
+        if (amigo == null)
+            erros += "> O Amigo é obrigatório!\n";
+
+        if (revista == null)
+            erros += "> A Revista é obrigatória!\n";
+
+        if (revista.StatusDeEmprestimo != "Disponível")
+            erros += "> A Revista não está disponível para empréstimo!\n";
+
+        return erros;
+    }
+
+    public DateTime ObterDataDeDevolucao()
+    {
+        return DataIniciodoEmprestimo.AddDays(revista.Caixa.DiasDeEmprestimoMaximo);
+    }
+
+    public bool EmprestimoEstaAtrasado(Emprestimo emprestimo)
+    {
+        if (emprestimo.DataDevolucao < DateTime.Now)
+            return true;
+        return false;
+    }
+
+    public void RegistrarDevolucao()
+    {
+        StatusDeEmprestimo = "Concluído";
+        revista.Devolver();
+    }
+
+    public void RegistrarAtraso()
+    {
+
+        StatusDeEmprestimo = "Atrasado";
+    }
+
+    public void RegistrarEmprestimo()
+    {
+        StatusDeEmprestimo = "Aberto";
+        revista.Emprestar();
+    }
 }
