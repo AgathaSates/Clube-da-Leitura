eaf0fdb43406e536f3993cf5c97d71067c236255
Agatha Sates <satesagatha@gmail.com>
2025-04-14 05:50:04 -0300

    Refactor: Alterado local das instâncias

--- Clube-da-Leitura.ConsoleApp/Utilitarios/TelaPincipal.cs
+++ Clube-da-Leitura.ConsoleApp/Utilitarios/TelaPincipal.cs
@@ -1,22 +1,75 @@
-﻿namespace Clube_da_Leitura.ConsoleApp.Utilitarios;
+﻿using Clube_da_Leitura.ConsoleApp.Compartilhado;
+using Clube_da_Leitura.ConsoleApp.ModuloAmigo;
+using Clube_da_Leitura.ConsoleApp.ModuloCaixa;
+using Clube_da_Leitura.ConsoleApp.ModuloEmprestimo;
+using Clube_da_Leitura.ConsoleApp.ModuloRevista;
 
-class telaPincipal
+namespace Clube_da_Leitura.ConsoleApp.Utilitarios;
+
+class TelaPincipal
 {
+    public readonly TelaAmigo telaAmigo;
+    public readonly TelaCaixa telaCaixa;
+    public readonly TelaRevista telaRevista;
+    public readonly TelaEmprestimo telaEmprestimo;
+
+    RepositorioAmigo repositorioAmigo;
+    RepositorioCaixa repositorioCaixa;
+    RepositorioRevista repositorioRevista;
+    RepositorioEmprestimo repositorioEmprestimo;
+
+
+    public TelaPincipal()
+    {
+        repositorioAmigo = new RepositorioAmigo();
+        repositorioCaixa = new RepositorioCaixa();
+        repositorioRevista = new RepositorioRevista();
+        repositorioEmprestimo = new RepositorioEmprestimo();
+
+
+        telaAmigo = new TelaAmigo(repositorioAmigo);
+        telaCaixa = new TelaCaixa(repositorioCaixa);
+        telaRevista = new TelaRevista(repositorioRevista, repositorioCaixa, telaCaixa);
+        telaEmprestimo = new TelaEmprestimo(repositorioEmprestimo, repositorioAmigo, repositorioRevista,telaAmigo,telaRevista);
+    }
+
     public void ApresentarMenuPrincipal()
     {
-        Console.Clear();
-        Console.WriteLine("╔════════════════════════════════╗");
-        Console.WriteLine("║        Clube da Leitura        ║");
-        Console.WriteLine("║════════════════════════════════║");
-        Console.WriteLine("║ Bem-vindo ao Clube da Leitura! ║");
-        Console.WriteLine("║════════════════════════════════║");
-        Console.WriteLine("║ 1. Gerenciar Amigos.           ║");
-        Console.WriteLine("║ 2. Gerenciar Revistas.         ║");
-        Console.WriteLine("║ 3. Gerenciar Caixas.           ║");
-        Console.WriteLine("║ 4. Gerenciar Empréstimos.      ║");
-        Console.WriteLine("║ 5. Sair do Clube.              ║");
-        Console.WriteLine("╚════════════════════════════════╝");
-        Console.Write("> Digite uma opção: ");
+        while (true)
+        {
+            Console.Title = "Clube da Leitura";
+
+
+            Console.Clear();
+            Console.WriteLine("╔════════════════════════════════╗");
+            Console.WriteLine("║        Clube da Leitura        ║");
+            Console.WriteLine("║════════════════════════════════║");
+            Console.WriteLine("║ Bem-vindo ao Clube da Leitura! ║");
+            Console.WriteLine("║════════════════════════════════║");
+            Console.WriteLine("║ 1. Gerenciar Amigos.           ║");
+            Console.WriteLine("║ 2. Gerenciar Revistas.         ║");
+            Console.WriteLine("║ 3. Gerenciar Caixas.           ║");
+            Console.WriteLine("║ 4. Gerenciar Empréstimos.      ║");
+            Console.WriteLine("║ 5. Sair do Clube.              ║");
+            Console.WriteLine("╚════════════════════════════════╝");
+            Console.Write("> Digite uma opção: ");
+            string opcaomenu = Console.ReadLine().Trim();
+
+            switch (opcaomenu)
+            {
+                case "1": telaAmigo.ApresentarMenu(); break;
+
+                case "2": telaRevista.ApresentarMenu(); break;
+
+                case "3": telaCaixa.ApresentarMenu(); break;
+
+                case "4": telaEmprestimo.ApresentarMenu(); break;
+
+                case "5": ApresentarSairDoClube(); return;
+
+                default: Notificador.ApresentarOpcaoInvalida(); break;
+            }
+        }
     }
 
     public void ApresentarSairDoClube()
@@ -26,10 +79,5 @@ class telaPincipal
         Console.WriteLine("║  Obrigado por usar o Clube da Leitura!  ║");
         Console.WriteLine("║              Até a próxima!             ║");
         Console.WriteLine("╚═════════════════════════════════════════╝");
-    }
-
-    public void ApresentarOpçaoInvalida()
-    {
-        Console.WriteLine("Opção inválida! Digite novamente: ");
     }
 }
