de4bff9504350b33241e71a643cadfea562f395e
Agatha Sates <satesagatha@gmail.com>
2025-04-14 00:21:34 -0300

    chore: Ajustado nome das variáveis

--- Clube-da-Leitura.ConsoleApp/ModuloRevista/Revista.cs
+++ Clube-da-Leitura.ConsoleApp/ModuloRevista/Revista.cs
@@ -1,58 +1,67 @@
-﻿using Clube_da_Leitura.ConsoleApp.ModuloCaixa;
+﻿using Clube_da_Leitura.ConsoleApp.Compartilhado;
+using Clube_da_Leitura.ConsoleApp.ModuloCaixa;
 
 namespace Clube_da_Leitura.ConsoleApp.ModuloRevista;
 class Revista
 {
-    public int id;
-    public string titulo;
-    public int numeroDaEdicao;
-    public int anoDaPublicacao;
-    public string statusDeEmprestimo;
-    public Caixa caixa;
-
-    //● Não pode haver revistas com mesmo título e edição
+    public int Id;
+    public string Titulo;
+    public int NumeroDaEdicao;
+    public int AnoDaPublicacao;
+    public string StatusDeEmprestimo;
+    public Caixa Caixa;
 
     public Revista(string titulo, int numeroDaEdicao, int anoDaPublicacao, Caixa caixa)
     {
-        this.titulo = titulo;
-        this.numeroDaEdicao = numeroDaEdicao;
-        this.anoDaPublicacao = anoDaPublicacao;
-        statusDeEmprestimo = "Disponível";
-        this.caixa = caixa;
+        Id = GeradorDeIDs.GerarIdRevista();
+        Titulo = titulo;
+        NumeroDaEdicao = numeroDaEdicao;
+        AnoDaPublicacao = anoDaPublicacao;
+        StatusDeEmprestimo = "Disponível";
+        Caixa = caixa;
     }
-    
+
+    public Revista(int id, string titulo, int numeroDaEdicao, int anoDaPublicacao, Caixa caixa)
+    {
+        Id = id;
+        Titulo = titulo;
+        NumeroDaEdicao = numeroDaEdicao;
+        AnoDaPublicacao = anoDaPublicacao;
+        StatusDeEmprestimo = "Disponível";
+        Caixa = caixa;
+    }
+
     public string Validar()
     {
         int tamanhoMaximoTitulo = 100;
         int tamanhoMinimoTitulo = 2;
         string erros = "";
 
-        if (string.IsNullOrWhiteSpace(titulo))
+        if (string.IsNullOrWhiteSpace(Titulo))
             erros += "> O Título é obrigatório!\n";
 
-        else if (titulo.Length < tamanhoMinimoTitulo || titulo.Length > tamanhoMaximoTitulo)
+        else if (Titulo.Length < tamanhoMinimoTitulo || Titulo.Length > tamanhoMaximoTitulo)
             erros += "> O Título deve ter entre 2 e 100 caracteres\n";
 
-        if (numeroDaEdicao <= 0)
+        if (NumeroDaEdicao <= 0)
             erros += "> O Número da Edição deve ser um número positivo\n";
 
-        if (anoDaPublicacao < 1800 || anoDaPublicacao > DateTime.Now.Year)
+        if (AnoDaPublicacao < 1800 || AnoDaPublicacao > DateTime.Now.Year)
             erros += "> O Ano da Publicação deve ser um ano válido\n";
 
-        if (caixa == null)
+        if (Caixa == null)
             erros += "> A Caixa é obrigatória!\n";
-
 
         return erros;
     }
 
     public void Emprestar()
     {
-        statusDeEmprestimo = "Emprestada";
+        StatusDeEmprestimo = "Emprestada";
     }
 
     public void Devolver()
     {
-        statusDeEmprestimo = "Disponível";
+        StatusDeEmprestimo = "Disponível";
     }
 }
